8d70c867c6858c75204f37bca2816dbfef334da8
pradipbade7 <pradipbade7@gmail.com>
2021-12-26 13:53:19 +0200

    Final commit

--- UrlScanner/Controllers/UrlController.cs
+++ UrlScanner/Controllers/UrlController.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UrlScanner.Services;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UrlScanner.Controllers
 {
--- UrlScanner/Services/UrlService.cs
+++ UrlScanner/Services/UrlService.cs
@@ -3,53 +3,50 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-//using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace UrlScanner.Services
 {
     public class UrlService
     {
-        public static List<string> DomainExtentions;
+        private static List<string> DomainExtentions;
         public UrlService()
         {
-            string json = File.ReadAllText("domainext.json");
+            string json = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Extentions/domainext.json"));
             DomainExtentions = JsonConvert.DeserializeObject<List<string>>(json);
         }
         public string UrlFromTextService(string text)
         {
-            text = text.Replace("\"", " ").Replace("</", " ");
+            text = text.Replace("\"", " ").Replace("</", " ").Replace(");", " ");
             List<string> urls = new List<string>();
             var linkParser = new Regex(@"\b((?:(?:https?):\/\/)|[\w/\-?=%.]+\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            Uri resultUri;
             foreach (Match m in linkParser.Matches(text))
             {
                 if (CheckUrl(m.Value, out string urltxt))
                 {
                     urls.Add(urltxt);
                 }
-                //if (CheckValidUrl(m.Value, out resultUri))
-                //{
-                //    urls.Add(CheckUrlScheme(m.Value));
-                //}
-
             }
             return JsonConvert.SerializeObject(urls.Distinct().ToList());
         }
-        private string CheckUrlScheme(string url)
-        {
-            if (!Regex.IsMatch(url, @"^https?:\/\/", RegexOptions.IgnoreCase))
-                url = "http://" + url;
-            return url;
-        }
-
         private bool CheckUrl(string url, out string urltxt)
         {
             try
             {
+                #region CheckEmail
+                const string EmailPattern = @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                 + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                 + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                 + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+
+                Regex remail = new Regex(EmailPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                if (remail.IsMatch(url))
+                {
+                    urltxt = null;
+                    return false;
+                }
+                #endregion
                 if (!Regex.IsMatch(url, @"^https?:\/\/", RegexOptions.IgnoreCase))
                     url = "http://" + url;
                 url = HttpUtility.UrlEncode(url);
@@ -74,28 +71,5 @@ namespace UrlScanner.Services
 
         }
 
-        private bool CheckValidUrl(string url, out Uri resultURI)
-        {
-            try
-            {
-                if (!Regex.IsMatch(url, @"^https?:\/\/", RegexOptions.IgnoreCase))
-                    url = "http://" + url;
-                if (Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out resultURI))
-                {
-                    var uri = new Uri(url);
-                    var hostsplit = uri.Host.Split('.');
-                    string ext = hostsplit[hostsplit.Length - 1];
-                    if (DomainExtentions.Contains(ext.ToUpper()))
-                        return true;
-                }
-                return false;
-            }
-            catch (Exception)
-            {
-                resultURI = null;
-                return false;
-            }
-
-        }
     }
 }
